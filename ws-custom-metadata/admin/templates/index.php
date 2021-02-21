00a735c09399395755d0e7d5730978e3f134e0e1
Word-Salad <79082725+Word-Salad@users.noreply.github.com>
2021-02-21 00:45:44 -0700

    Update index.php

--- ws-custom-metadata/admin/templates/index.php
+++ ws-custom-metadata/admin/templates/index.php
@@ -12,22 +12,38 @@
               $user_meta_3 = $_POST['user_meta_3'];
               $user_meta_4 = $_POST['user_meta_4'];
             
-            update_user_meta($user_id , '$user_meta_1', $user_age);
-            update_user_meta($user_id , '$user_meta_2', $user_age);
-            update_user_meta($user_id , '$user_meta_3', $user_age);
-            update_user_meta($user_id , '$user_meta_4', $user_age);
+            update_user_meta($user_id , 'user_meta_1', $user_meta_1);
+            update_user_meta($user_id , 'user_meta_2', $user_meta_2);
+            update_user_meta($user_id , 'user_meta_3', $user_meta_3);
+            update_user_meta($user_id , 'user_meta_4', $user_meta_4);
 
-            $meta_user_age = get_user_meta( $user_id, 'user_age', true );
-            echo $meta_user_age;
+            $user_meta_1 = get_user_meta( $user_id, 'user_meta_1', true );
+            $user_meta_2 = get_user_meta( $user_id, 'user_meta_2', true );
+            $user_meta_3 = get_user_meta( $user_id, 'user_meta_3', true );
+            $user_meta_4 = get_user_meta( $user_id, 'user_meta_4', true );
 
         }
     ?>
 
     <form method="POST">
-    <input type="text" placeholder="1" name="user_meta_1">
-    <input type="text" placeholder="2" name="user_meta_2">
-    <input type="text" placeholder="3" name="user_meta_3">
-    <input type="text" placeholder="4" name="user_meta_4">
+        <ul>
+            <li>
+                <input type="text" placeholder="<? echo get_user_meta( get_current_user_id(), 'user_meta_1', true ); ?>" name="user_meta_1">
+              <input type="submit" name="form_submit" method="post">
+              </li>
+            <li>
+                <input type="text" placeholder="<? echo get_user_meta( get_current_user_id(), 'user_meta_2', true ); ?>" name="user_meta_2">
+              <input type="submit" name="form_submit" method="post">
+              </li>
+            <li>
+                <input type="text" placeholder="<? echo get_user_meta( get_current_user_id(), 'user_meta_3', true ); ?>" name="user_meta_3">
+              <input type="submit" name="form_submit" method="post">
+              </li>
+            <li>
+                <input type="text" placeholder="<? echo get_user_meta( get_current_user_id(), 'user_meta_4', true ); ?>" name="user_meta_4">
+              <input type="submit" name="form_submit" method="post">
+              </li>
+        </ul>
     <input type="submit" name="form_submit" method="post">
     </form>
 
