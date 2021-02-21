3b15c11663706a5956f6d47e0380bc995aaa5e5f
Word-Salad <79082725+Word-Salad@users.noreply.github.com>
2021-02-21 00:54:45 -0700

    Update index.php

--- ws-custom-metadata/admin/templates/index.php
+++ ws-custom-metadata/admin/templates/index.php
@@ -3,45 +3,52 @@
     <h1>WS Custom Metadata</h1>
 
     <?php
-        if( isset( $_POST['form_submit'] ) ) {
-
+        if( isset( $_POST['post_user_meta_1'] ) ) {
             $user_id = get_current_user_id();
-              
-              $user_meta_1 = $_POST['user_meta_1'];
-              $user_meta_2 = $_POST['user_meta_2'];
-              $user_meta_3 = $_POST['user_meta_3'];
-              $user_meta_4 = $_POST['user_meta_4'];
-            
+            $user_meta_1 = $_POST['user_meta_1'];
             update_user_meta($user_id , 'user_meta_1', $user_meta_1);
-            update_user_meta($user_id , 'user_meta_2', $user_meta_2);
-            update_user_meta($user_id , 'user_meta_3', $user_meta_3);
-            update_user_meta($user_id , 'user_meta_4', $user_meta_4);
-
             $user_meta_1 = get_user_meta( $user_id, 'user_meta_1', true );
+        }
+      
+      if( isset( $_POST['post_user_meta_2'] ) ) {
+            $user_id = get_current_user_id();
+            $user_meta_2 = $_POST['user_meta_2'];
+            update_user_meta($user_id , 'user_meta_2', $user_meta_2);
             $user_meta_2 = get_user_meta( $user_id, 'user_meta_2', true );
+        }
+      
+      if( isset( $_POST['post_user_meta_3'] ) ) {
+            $user_id = get_current_user_id();
+            $user_meta_3 = $_POST['user_meta_3'];
+            update_user_meta($user_id , 'user_meta_3', $user_meta_3);
             $user_meta_3 = get_user_meta( $user_id, 'user_meta_3', true );
+        }
+      
+      if( isset( $_POST['post_user_meta_4'] ) ) {
+            $user_id = get_current_user_id();
+            $user_meta_4 = $_POST['user_meta'];
+            update_user_meta($user_id , 'user_meta_4', $user_meta_4);
             $user_meta_4 = get_user_meta( $user_id, 'user_meta_4', true );
-
         }
     ?>
 
     <form method="POST">
         <ul>
             <li>
-                <input type="text" placeholder="<? echo get_user_meta( get_current_user_id(), 'user_meta_1', true ); ?>" name="user_meta_1">
-              <input type="submit" name="form_submit" method="post">
+              <input type="text" placeholder="<? echo get_user_meta( get_current_user_id(), 'user_meta_1', true ); ?>" name="user_meta_1">
+              <input type="submit" name="post_user_meta_1" method="post">
               </li>
             <li>
-                <input type="text" placeholder="<? echo get_user_meta( get_current_user_id(), 'user_meta_2', true ); ?>" name="user_meta_2">
-              <input type="submit" name="form_submit" method="post">
+              <input type="text" placeholder="<? echo get_user_meta( get_current_user_id(), 'user_meta_2', true ); ?>" name="user_meta_2">
+              <input type="submit" name="post_user_meta_2" method="post">
               </li>
             <li>
-                <input type="text" placeholder="<? echo get_user_meta( get_current_user_id(), 'user_meta_3', true ); ?>" name="user_meta_3">
-              <input type="submit" name="form_submit" method="post">
+              <input type="text" placeholder="<? echo get_user_meta( get_current_user_id(), 'user_meta_3', true ); ?>" name="user_meta_3">
+              <input type="submit" name="post_user_meta_3" method="post">
               </li>
             <li>
-                <input type="text" placeholder="<? echo get_user_meta( get_current_user_id(), 'user_meta_4', true ); ?>" name="user_meta_4">
-              <input type="submit" name="form_submit" method="post">
+              <input type="text" placeholder="<? echo get_user_meta( get_current_user_id(), 'user_meta_4', true ); ?>" name="user_meta_4">
+              <input type="submit" name="post_user_meta_4" method="post">
               </li>
         </ul>
     <input type="submit" name="form_submit" method="post">
