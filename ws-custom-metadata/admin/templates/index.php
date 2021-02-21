ab519817aab49e35e4a4d2656fe80d606eceff12
Word-Salad <79082725+Word-Salad@users.noreply.github.com>
2021-02-20 22:40:27 -0700

    Add user meta 1-4

--- ws-custom-metadata/admin/templates/index.php
+++ ws-custom-metadata/admin/templates/index.php
@@ -5,9 +5,17 @@
     <?php
         if( isset( $_POST['form_submit'] ) ) {
 
-            $user_age = $_POST['user_age'];
             $user_id = get_current_user_id();
-            update_user_meta($user_id , 'user_age', $user_age);
+              
+              $user_meta_1 = $_POST['user_meta_1'];
+              $user_meta_2 = $_POST['user_meta_2'];
+              $user_meta_3 = $_POST['user_meta_3'];
+              $user_meta_4 = $_POST['user_meta_4'];
+            
+            update_user_meta($user_id , '$user_meta_1', $user_age);
+            update_user_meta($user_id , '$user_meta_2', $user_age);
+            update_user_meta($user_id , '$user_meta_3', $user_age);
+            update_user_meta($user_id , '$user_meta_4', $user_age);
 
             $meta_user_age = get_user_meta( $user_id, 'user_age', true );
             echo $meta_user_age;
@@ -16,7 +24,10 @@
     ?>
 
     <form method="POST">
-    <input type="text" placeholder="Your Age Plez" name="user_age">
+    <input type="text" placeholder="1" name="user_meta_1">
+    <input type="text" placeholder="2" name="user_meta_2">
+    <input type="text" placeholder="3" name="user_meta_3">
+    <input type="text" placeholder="4" name="user_meta_4">
     <input type="submit" name="form_submit" method="post">
     </form>
 
